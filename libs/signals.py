f9d1c51284bc348d5dcb8921556f3de4f676c191
MinsooYeo <minsooyeo119112@gmail.com>
2021-12-14 10:02:48 +0900

    기본 파일 추가

---
+++ libs/signals.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy import signal
+from scipy.interpolate import interp1d
+
+
+def interpolation_1d(data, outPutLen, interpol_type='linear'):
+    f1 = interp1d(range(0, len(data)), data, kind=interpol_type)
+    inter1Peaks = f1(np.linspace(0, len(data) - 1, num=outPutLen))
+    return inter1Peaks
+
+
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
